7adb161c1c203dff3a726ee3f477f32c3752824b
Ender Watts <103525727+ewatgar@users.noreply.github.com>
2024-02-10 22:48:33 +0100

    Crear prefab card

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer CardBack, CardFlag;
+    public Controller controller;
+    private bool _bIsCurrentlyFlipped = false;
+    private int _id;
+    private Vector2 _size;
+
+    public bool IsCurrentlyFlipped {
+        get{ return _bIsCurrentlyFlipped; }
+    }
+
+    public int Id {
+        get{ return _id; }
+    }
+
+    public Vector2 Size {
+        get{ return _size; }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Flip(){
+        CardBack.gameObject.SetActive(false);
+        _bIsCurrentlyFlipped = true;
+    }
+
+    public void Unflip(){
+        CardBack.gameObject.SetActive(true);
+        _bIsCurrentlyFlipped = false;
+    }
+
+    void SetCard(int id, Sprite image){
+        _id = id;
+        CardFlag.sprite = image;
+        _size = CardBack.sprite.bounds.size;
+    }
+
+    void OnMouseDown(){
+        Debug.Log(IsCurrentlyFlipped);
+        if (!IsCurrentlyFlipped){
+            Flip();
+        }
+    }
+}
